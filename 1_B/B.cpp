513441b94a2b9530d42ea7fed59e85afb83cf243
julia-tarsv <145669523+julia-tarsv@users.noreply.github.com>
2023-11-06 22:22:06 +0300

    Update B.cpp

--- 1_B/B.cpp
+++ 1_B/B.cpp
@@ -4,6 +4,9 @@
 #include <iostream>
 #include <vector>
 
+/* вычислить среднее геометрическое на отрезке от l до
+r по нескольким запросам */
+
 int main() {
   size_t number_of_mandrakes;
   std::cin >> number_of_mandrakes;
@@ -14,21 +17,19 @@ int main() {
     mandrakes[i] = log2(loudness);
   }
   std::vector<double> prefix_sum(number_of_mandrakes + 1, 0);
-  prefix_sum[1] = mandrakes[0];
-  for (size_t i = 2; i <= number_of_mandrakes; ++i) {
+  for (size_t i = 1; i <= number_of_mandrakes; ++i) {
     prefix_sum[i] = prefix_sum[i - 1] + mandrakes[i - 1];
   }
   size_t requests;
   std::cin >> requests;
   const int kPrecision = 8;
   for (size_t i = 0; i < requests; ++i) {
-    double left;
-    double right;
+    int left;
+    int right;
     std::cin >> left >> right;
-    ++left;
-    ++right;
-    double power = (prefix_sum[int(right)] - prefix_sum[int(left) - 1]) /
-                   (right - left + 1);
+
+    double power =
+        ((prefix_sum[right + 1] - prefix_sum[left]) / (right - left + 1));
     double mid_loudness = pow(2, power);
     std::cout << std::setprecision(kPrecision) << mid_loudness << std::endl;
   }
