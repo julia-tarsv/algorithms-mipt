e6ef0afa417ca73e8c380d250c0f606f488dd2ef
julia-tarsv <145669523+julia-tarsv@users.noreply.github.com>
2023-12-13 23:08:52 +0300

    Update 3F.cpp

--- 3_F/3F.cpp
+++ 3_F/3F.cpp
@@ -6,84 +6,91 @@ struct Node {
   std::vector<int> sort_elem;
 };
 
-int Get(std::vector<Node>& tree, int vv, std::pair<size_t, size_t> tt,
-        std::pair<size_t, size_t> lr, std::pair<int, int> bound) {
-  if (tt.first > lr.second || lr.first > tt.second) {
+int Get(std::vector<Node>& tree, int vertex, std::pair<size_t, size_t> tree_l_r,
+        std::pair<size_t, size_t> left_right, std::pair<int, int> bounderies) {
+  if (tree_l_r.first > left_right.second ||
+      left_right.first > tree_l_r.second) {
     return 0;
   }
 
-  if (lr.first <= tt.first && tt.second <= lr.second) {
-    size_t ans1 = upper_bound(tree[vv].sort_elem.begin(),
-                              tree[vv].sort_elem.end(), bound.second) -
-                  tree[vv].sort_elem.begin();
-    size_t ans2 = lower_bound(tree[vv].sort_elem.begin(),
-                              tree[vv].sort_elem.end(), bound.first) -
-                  tree[vv].sort_elem.begin();
+  if (left_right.first <= tree_l_r.first &&
+      tree_l_r.second <= left_right.second) {
+    size_t ans1 = upper_bound(tree[vertex].sort_elem.begin(),
+                              tree[vertex].sort_elem.end(), bounderies.second) -
+                  tree[vertex].sort_elem.begin();
+    size_t ans2 = lower_bound(tree[vertex].sort_elem.begin(),
+                              tree[vertex].sort_elem.end(), bounderies.first) -
+                  tree[vertex].sort_elem.begin();
     return ans1 - ans2;
   }
 
-  size_t tm = (tt.first + tt.second) / 2;
-  return Get(tree, 2 * vv, {tt.first, tm}, {lr.first, lr.second}, bound) +
-         Get(tree, 2 * vv + 1, {tm + 1, tt.second}, {lr.first, lr.second},
-             bound);
+  size_t tree_mid = (tree_l_r.first + tree_l_r.second) / 2;
+  return Get(tree, 2 * vertex, {tree_l_r.first, tree_mid},
+             {left_right.first, left_right.second}, bounderies) +
+         Get(tree, 2 * vertex + 1, {tree_mid + 1, tree_l_r.second},
+             {left_right.first, left_right.second}, bounderies);
 }
 
-void Build(std::vector<Node>& tree, std::vector<int>& mas, int vv, size_t tl,
-           size_t tr) {
-  if (tl == tr) {
-    tree[vv].sort_elem.push_back(mas[tl]);
+void Build(std::vector<Node>& tree, std::vector<int>& mas, int vertex,
+           size_t tree_left, size_t tree_right) {
+  if (tree_left == tree_right) {
+    tree[vertex].sort_elem.push_back(mas[tree_left]);
     return;
   }
 
-  size_t tm = (tl + tr) / 2;
-  Build(tree, mas, 2 * vv, tl, tm);
-  Build(tree, mas, 2 * vv + 1, tm + 1, tr);
+  size_t tree_mid = (tree_left + tree_right) / 2;
+  Build(tree, mas, 2 * vertex, tree_left, tree_mid);
+  Build(tree, mas, 2 * vertex + 1, tree_mid + 1, tree_right);
 
-  size_t ind1 = 0;
-  size_t ind2 = 0;
+  size_t index1 = 0;
+  size_t index2 = 0;
 
-  while (ind1 < tree[2 * vv].sort_elem.size() &&
-         ind2 < tree[2 * vv + 1].sort_elem.size()) {
-    if (tree[2 * vv].sort_elem[ind1] <= tree[2 * vv + 1].sort_elem[ind2]) {
-      tree[vv].sort_elem.push_back(tree[2 * vv].sort_elem[ind1]);
-      ++ind1;
+  while (index1 < tree[2 * vertex].sort_elem.size() &&
+         index2 < tree[2 * vertex + 1].sort_elem.size()) {
+    if (tree[2 * vertex].sort_elem[index1] <=
+        tree[2 * vertex + 1].sort_elem[index2]) {
+      tree[vertex].sort_elem.push_back(tree[2 * vertex].sort_elem[index1]);
+      ++index1;
     } else {
-      tree[vv].sort_elem.push_back(tree[2 * vv + 1].sort_elem[ind2]);
-      ++ind2;
+      tree[vertex].sort_elem.push_back(tree[2 * vertex + 1].sort_elem[index2]);
+      ++index2;
     }
   }
-  while (ind1 < tree[2 * vv].sort_elem.size()) {
-    tree[vv].sort_elem.push_back(tree[2 * vv].sort_elem[ind1]);
-    ++ind1;
+  while (index1 < tree[2 * vertex].sort_elem.size()) {
+    tree[vertex].sort_elem.push_back(tree[2 * vertex].sort_elem[index1]);
+    ++index1;
   }
-  while (ind2 < tree[2 * vv + 1].sort_elem.size()) {
-    tree[vv].sort_elem.push_back(tree[2 * vv + 1].sort_elem[ind2]);
-    ++ind2;
+  while (index2 < tree[2 * vertex + 1].sort_elem.size()) {
+    tree[vertex].sort_elem.push_back(tree[2 * vertex + 1].sort_elem[index2]);
+    ++index2;
   }
 }
 
-int main() {
+void Acceleration() {
   std::ios::sync_with_stdio(false);
   std::cin.tie(0);
   std::cout.tie(0);
+}
 
+int main() {
+  Acceleration();
   size_t num;
-  size_t req;
-  std::cin >> num >> req;
+  size_t requests;
+  std::cin >> num >> requests;
   std::vector<Node> tree(4 * num);
   std::vector<int> mas(num);
   for (size_t i = 0; i < num; ++i) {
     std::cin >> mas[i];
   }
   Build(tree, mas, 1, 0, num - 1);
-  for (size_t i = 0; i < req; ++i) {
+  for (size_t i = 0; i < requests; ++i) {
     int lr1;
     int lr2;
-    int bound1;
-    int bound2;
-    std::cin >> lr1 >> lr2 >> bound1 >> bound2;
+    int boundery1;
+    int boundery2;
+    std::cin >> lr1 >> lr2 >> boundery1 >> boundery2;
     std::cout << Get(tree, 1, {0, num - 1}, {lr1 - 1, lr2 - 1},
-                     {bound1, bound2})
+                     {boundery1, boundery2})
               << "\n";
   }
 }
