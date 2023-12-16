a6e23e6d16c775d39d60253f5c93fe2c13ea6189
julia-tarsv <145669523+julia-tarsv@users.noreply.github.com>
2023-12-17 00:40:09 +0300

    Update main.cpp

--- 2_F/main.cpp
+++ 2_F/main.cpp
@@ -2,40 +2,49 @@
 #include <string>
 #include <vector>
 
+/*
+    insert x — вставить целое число x в кучу;
+    getMin — вывести значение минимального элемента в куче;
+    extractMin — удалить минимальный элемент из кучи, выводить его не нужно;
+    decreaseKey i Δ — уменьшить число, вставленное на i-м запросе, на целое
+    число Δ≥0
+*/
+
 class BinHeap {
-public:
-  void Insert(long long element, int ind);
+ public:
+  void Insert(long long element, int index);
   void ExtractMin();
   void DecreaseKey(int index, long long delta);
   long long GetMin();
 
   BinHeap() : tree_(1) {}
 
-private:
+ private:
   std::vector<std::pair<long long, int>> tree_;
 
-  void SiftUp(int ind) {
-    if (ind == 1) {
+  void SiftUp(int index) {
+    if (index == 1) {
       return;
     }
-    int pred = ind >> 1;
-    if (tree_[ind].first < tree_[pred].first) {
-      std::swap(tree_[ind], tree_[pred]);
-      SiftUp(pred);
+    int previous = index / 2;
+    if (tree_[index].first < tree_[previous].first) {
+      std::swap(tree_[index], tree_[previous]);
+      SiftUp(previous);
     }
   }
 
-  void SiftDown(int ind) {
-    if (ind << 1 >= int(tree_.size())) {
+  void SiftDown(int index) {
+    if (index * 2 >= (int)tree_.size()) {
       return;
     }
-    int uu = ind << 1;
-    if (uu + 1 < int(tree_.size()) && tree_[uu + 1].first < tree_[uu].first) {
-      ++uu;
+    int vertex = index << 1;
+    if (vertex + 1 < (int)tree_.size() &&
+        tree_[vertex + 1].first < tree_[vertex].first) {
+      ++vertex;
     }
-    if (tree_[uu].first < tree_[ind].first) {
-      std::swap(tree_[uu], tree_[ind]);
-      SiftDown(uu);
+    if (tree_[vertex].first < tree_[index].first) {
+      std::swap(tree_[vertex], tree_[index]);
+      SiftDown(vertex);
     }
   }
 };
@@ -51,33 +60,40 @@ void BinHeap::ExtractMin() {
   SiftDown(1);
 }
 
-void BinHeap::Insert(long long element, int ind) {
-  tree_.emplace_back(std::make_pair(element, ind));
-  SiftUp(int(tree_.size()) - 1);
+void BinHeap::Insert(long long element, int index) {
+  tree_.push_back({element, index});
+  SiftUp(tree_.size() - 1);
 }
 
 void BinHeap::DecreaseKey(int index, long long delta) {
-  int ind1 = 0;
-  for (int i = 1; i < int(tree_.size()); ++i) {
+  int index1 = 0;
+  for (size_t i = 1; i < tree_.size(); ++i) {
     if (tree_[i].second == index) {
-      ind1 = i;
+      index1 = i;
       break;
     }
   }
-  tree_[ind1].first -= delta;
-  SiftUp(ind1);
+  tree_[index1].first -= delta;
+  SiftUp(index1);
 }
 
-int main() {
+void Acceleration() {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(0);
   std::cout.tie(0);
-  int requests;
+}
+
+int main() {
+  Acceleration();
+  size_t requests;
   std::cin >> requests;
+
   BinHeap heap;
-  for (int i = 0; i < requests; ++i) {
+
+  for (size_t i = 0; i < requests; ++i) {
     std::string str;
     std::cin >> str;
+
     if (str == "insert") {
       long long elem;
       std::cin >> elem;
@@ -87,10 +103,10 @@ int main() {
     } else if (str == "extractMin") {
       heap.ExtractMin();
     } else {
-      int ind;
+      int index;
       long long delta;
-      std::cin >> ind >> delta;
-      heap.DecreaseKey(ind, delta);
+      std::cin >> index >> delta;
+      heap.DecreaseKey(index, delta);
     }
   }
 }
