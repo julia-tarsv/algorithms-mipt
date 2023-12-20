0d2929b7e46fe14eaaf58740fff908d757e562cf
julia-tarsv <145669523+julia-tarsv@users.noreply.github.com>
2023-12-20 22:40:34 +0300

    Update main.cpp

--- 4_B/main.cpp
+++ 4_B/main.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 
 /*
-    add(i)  — добавить в множество машин тачку с разгоном в i миллисекунд (если
-    такая там уже есть, то Доминик отвлекся на семейные разговоры и подсунул вам
-    второй раз ту же машину); next(i)  — узнать машину с минимальным временем
+    add(i)  — добавить в множество машин тачку с разгоном в i миллисекунд.
+    next(i)  — узнать машину с минимальным временем
     разгона, не меньшим i. Если искомая машина отсутствует, необходимо вывести
     -1 и попросить Доминика быть внимательнее.
 */
 
 class AVLTree {
-private:
+ public:
+  AVLTree() : node_(nullptr) {}
+
+  void Insert(int key) { node_ = InsertHelp(node_, key); }
+  int Find(int key) { return FindHelp(node_, key); }
+
+ private:
   struct Node {
     int key;
     Node* left;
@@ -23,59 +28,44 @@ private:
   static Node* LeftBalance(Node* main);
   static Node* Balance(Node* main);
 
-  static Node* SmallRotateRight(Node* vert);
-  static Node* SmallRotateLeft(Node* vert);
-  static Node* BigRotateRight(Node* vert);
-  static Node* BigRotateLeft(Node* vert);
+  static Node* SmallRotateRight(Node* vertex);
+  static Node* SmallRotateLeft(Node* vertex);
+  static Node* BigRotateRight(Node* vertex);
+  static Node* BigRotateLeft(Node* vertex);
 
   Node* InsertHelp(Node*& main, int key);
 
   int FindHelp(Node* main, int key);
-  static void Height(Node*& vert);
+  static void IsHeight(Node*& vertex);
 
-public:
-  AVLTree() : node_(nullptr) {}
-  ~AVLTree() { Delete(node_); }
-
-  void Delete(Node*& vert) {
-    if (vert == nullptr) {
+ public:
+  void Delete(Node*& vertex) {
+    if (vertex == nullptr) {
       return;
     }
-    Delete(vert->right);
-    Delete(vert->left);
-    delete vert;
+    Delete(vertex->right);
+    Delete(vertex->left);
+    delete vertex;
   }
 
-  void Insert(int key) { node_ = InsertHelp(node_, key); }
-  int Find(int key) { return FindHelp(node_, key); }
+  ~AVLTree() { Delete(node_); }
 };
 
-void AVLTree::Height(Node*& vert) {
-  if (vert->left == nullptr && vert->right == nullptr) {
-    vert->height = 1;
-  } else if (vert->left == nullptr) {
-    vert->height = vert->right->height + 1;
-  } else if (vert->right == nullptr) {
-    vert->height = vert->left->height + 1;
-  } else {
-    vert->height = std::max(vert->left->height, vert->right->height) + 1;
-  }
+void AVLTree::IsHeight(Node*& vertex) {
+  size_t left_height;
+  size_t right_height;
+  left_height = (vertex->left == nullptr ? 0 : vertex->left->height);
+  right_height = (vertex->right == nullptr ? 0 : vertex->right->height);
+  vertex->height = std::max(left_height, right_height) + 1;
 }
 
 AVLTree::Node* AVLTree::LeftBalance(Node* main) {
   int left_height;
   int right_height;
 
-  if (main->left->left == nullptr) {
-    left_height = 0;
-  } else {
-    left_height = main->left->left->height;
-  }
-  if (main->left->right == nullptr) {
-    right_height = 0;
-  } else {
-    right_height = main->left->right->height;
-  }
+  left_height = (main->left->left == nullptr ? 0 : main->left->left->height);
+  right_height = (main->left->right == nullptr ? 0 : main->left->right->height);
+
   if (right_height - left_height > 0) {
     return BigRotateRight(main);
   }
@@ -87,7 +77,7 @@ AVLTree::Node* AVLTree::Balance(Node* main) {
   if (main == nullptr) {
     return main;
   }
-  Height(main);
+  IsHeight(main);
   int left_height = (main->left != nullptr ? main->left->height : 0);
   int right_height = (main->right != nullptr ? main->right->height : 0);
 
@@ -100,16 +90,10 @@ AVLTree::Node* AVLTree::Balance(Node* main) {
 
   if (right_height - left_height > 1) {
     if (main->right != nullptr) {
-      if (main->right->left == nullptr) {
-        left_height = 0;
-      } else {
-        left_height = main->right->left->height;
-      }
-      if (main->right->right == nullptr) {
-        right_height = 0;
-      } else {
-        right_height = main->right->right->height;
-      }
+      left_height =
+          (main->right->left == nullptr ? 0 : main->right->left->height);
+      right_height =
+          (main->right->right == nullptr ? 0 : main->right->right->height);
       if (left_height - right_height > 0) {
         return BigRotateLeft(main);
       }
@@ -132,50 +116,50 @@ AVLTree::Node* AVLTree::InsertHelp(Node*& main, int key) {
   return Balance(main);
 }
 
-AVLTree::Node* AVLTree::SmallRotateLeft(Node* vert) {
-  if (vert == nullptr) {
+AVLTree::Node* AVLTree::SmallRotateLeft(Node* vertex) {
+  if (vertex == nullptr) {
     return nullptr;
   }
-  if (vert->right == nullptr) {
-    return vert;
+  if (vertex->right == nullptr) {
+    return vertex;
   }
-  Node* right_son = vert->right;
-  vert->right = right_son->left;
-  Height(right_son);
-  Node* old_vert = vert;
-  vert = right_son;
-  vert->left = old_vert;
-  Height(vert);
-  return vert;
+  Node* right_son = vertex->right;
+  vertex->right = right_son->left;
+  IsHeight(right_son);
+  Node* old_vertex = vertex;
+  vertex = right_son;
+  vertex->left = old_vertex;
+  IsHeight(vertex);
+  return vertex;
 }
 
-AVLTree::Node* AVLTree::SmallRotateRight(Node* vert) {
-  if (vert == nullptr) {
+AVLTree::Node* AVLTree::SmallRotateRight(Node* vertex) {
+  if (vertex == nullptr) {
     return nullptr;
   }
-  if (vert->left == nullptr) {
-    return vert;
+  if (vertex->left == nullptr) {
+    return vertex;
   }
-  Node* left_son = vert->left;
-  vert->left = left_son->right;
-  Height(left_son);
-  Node* old_vert = vert;
-  vert = left_son;
-  vert->right = old_vert;
-  Height(vert);
-  return vert;
+  Node* left_son = vertex->left;
+  vertex->left = left_son->right;
+  IsHeight(left_son);
+  Node* old_vertex = vertex;
+  vertex = left_son;
+  vertex->right = old_vertex;
+  IsHeight(vertex);
+  return vertex;
 }
 
-AVLTree::Node* AVLTree::BigRotateLeft(Node* vert) {
-  Node* right_son = vert->right;
-  vert->right = SmallRotateRight(right_son);
-  return SmallRotateLeft(vert);
+AVLTree::Node* AVLTree::BigRotateLeft(Node* vertex) {
+  Node* right_son = vertex->right;
+  vertex->right = SmallRotateRight(right_son);
+  return SmallRotateLeft(vertex);
 }
 
-AVLTree::Node* AVLTree::BigRotateRight(Node* vert) {
-  Node* left_son = vert->left;
-  vert->left = SmallRotateLeft(left_son);
-  return SmallRotateRight(vert);
+AVLTree::Node* AVLTree::BigRotateRight(Node* vertex) {
+  Node* left_son = vertex->left;
+  vertex->left = SmallRotateLeft(left_son);
+  return SmallRotateRight(vertex);
 }
 
 int AVLTree::FindHelp(Node* main, int key) {
@@ -188,41 +172,44 @@ int AVLTree::FindHelp(Node* main, int key) {
   if (key > main->key) {
     return FindHelp(main->right, key);
   }
-  int ans = FindHelp(main->left, key);
-  if (ans != -1) {
-    return ans;
+  int result = FindHelp(main->left, key);
+  if (result != -1) {
+    return result;
   }
   return main->key;
 }
 
+void Acceleration() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+  std::cout.tie(0);
+}
+
 int main() {
-  size_t num;
-  std::cin >> num;
+  Acceleration();
+  size_t numbers;
+  std::cin >> numbers;
   AVLTree main;
-  char prev = '+';
-  int prev_res = 0;
+  char previous = '+';
+  int previous_result = 0;
   const int kMod = 1e9;
-  for (size_t i = 0; i < num; ++i) {
+
+  for (size_t i = 0; i < numbers; ++i) {
     char symbol;
     int key;
     std::cin >> symbol >> key;
     if (symbol == '+') {
-      if (prev == '+') {
-        if (main.Find(key) == key) {
-          continue;
-        }
-        main.Insert(key);
-      } else {
-        prev = '+';
-        if (main.Find(key + prev_res) == key) {
-          continue;
-        }
-        main.Insert((key + prev_res) % (kMod));
+      if (previous == '?') {
+        key = (key + previous_result) % kMod;
       }
+      if (main.Find(key) != key) {
+        main.Insert(key);
+      }
+      previous = '+';
     } else {
-      prev_res = main.Find(key);
-      std::cout << prev_res << "\n";
-      prev = '?';
+      previous_result = main.Find(key);
+      std::cout << previous_result << "\n";
+      previous = '?';
     }
   }
 }
