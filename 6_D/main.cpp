bb53458d678b0be9c9a3aa28a3139493e4edc88b
julia-tarsv <145669523+julia-tarsv@users.noreply.github.com>
2024-04-09 22:55:56 +0300

    Update main.cpp

--- 6_D/main.cpp
+++ 6_D/main.cpp
@@ -2,79 +2,88 @@
 #include <iostream>
 #include <vector>
 
-//найти компоненты сильной связанности
+// найти компоненты сильной связности
 
 class Graph {
-private:
-  size_t size_;
+ private:
   std::vector<std::vector<int>> graph_;
   std::vector<int> used_;
   std::vector<int> color_;
-  int timer_ = 0;
 
-public:
+ public:
+  int timer = 0;
   std::vector<std::pair<int, size_t>> time;
 
-  Graph(size_t size)
-      : size_(size), graph_(size), used_(size), color_(size), time(size - 1) {}
+  explicit Graph(size_t size)
+      : graph_(size), used_(size), color_(size), time(size - 1) {}
 
   void AddEdge(int vertex1, int vertex2);
 
-  void Dfs(size_t vertex);
+  std::vector<int>& GetNeighbours(size_t vertex);
 
-  void DfsCnt(size_t vertex, size_t cnt);
+  std::vector<int>& GetColor();
 
-  void TimeCnt();
-
-  void CountComponents();
+  std::vector<int>& GetUsed();
 };
 
 void Graph::AddEdge(int vertex1, int vertex2) {
   graph_[vertex1].push_back(vertex2);
 }
 
-void Graph::Dfs(size_t vertex) {
-  used_[vertex] = 1;
-  ++timer_;
-  for (auto elem : graph_[vertex]) {
-    if (used_[elem] == 0) {
-      Dfs(elem);
+void Dfs(Graph& graph, size_t vertex) {
+  graph.GetUsed()[vertex] = 1;
+  ++graph.timer;
+  for (auto elem : graph.GetNeighbours(vertex)) {
+    if (graph.GetUsed()[elem] == 0) {
+      Dfs(graph, elem);
     }
   }
-  used_[vertex] = 2;
-  time[vertex - 1] = {timer_++, vertex};
+
+  graph.GetUsed()[vertex] = 2;
+  graph.time[vertex - 1] = {graph.timer++, vertex};
 }
 
-void Graph::DfsCnt(size_t vertex, size_t cnt) {
-  color_[vertex] = cnt;
-  for (auto elem : graph_[vertex]) {
-    if (color_[elem] == 0) {
-      DfsCnt(elem, cnt);
+void DfsCnt(Graph& graph, size_t vertex, size_t cnt) {
+  graph.GetColor()[vertex] = cnt;
+  for (auto elem : graph.GetNeighbours(vertex)) {
+    if (graph.GetColor()[elem] == 0) {
+      DfsCnt(graph, elem, cnt);
     }
   }
 }
 
-void Graph::TimeCnt() {
-  for (size_t i = 1; i < size_; ++i) {
-    if (used_[i] == 0) {
-      Dfs(i);
+std::vector<int>& Graph::GetNeighbours(size_t vertex) { return graph_[vertex]; }
+
+std::vector<int>& Graph::GetColor() { return color_; }
+
+std::vector<int>& Graph::GetUsed() { return used_; }
+
+void TimeCnt(Graph& graph, size_t size) {
+  for (size_t i = 1; i < size; ++i) {
+    if (graph.GetUsed()[i] == 0) {
+      Dfs(graph, i);
     }
   }
-  std::sort(time.rbegin(), time.rend());
+
+  std::sort(graph.time.rbegin(), graph.time.rend());
 }
 
-void Graph::CountComponents() {
+std::vector<int> CountComponents(Graph& graph, size_t size) {
+  std::vector<int> res_components;
+
   size_t cnt = 1;
-  for (size_t i = 0; i < size_ - 1; ++i) {
-    if (color_[time[i].second] == 0) {
-      DfsCnt(time[i].second, cnt);
+  for (size_t i = 0; i < size - 1; ++i) {
+    if (graph.GetColor()[graph.time[i].second] == 0) {
+      DfsCnt(graph, graph.time[i].second, cnt);
       ++cnt;
     }
   }
-  std::cout << cnt - 1 << "\n";
-  for (size_t i = 1; i < size_; ++i) {
-    std::cout << color_[i] << ' ';
+
+  for (size_t i = 1; i < size; ++i) {
+    res_components.push_back(graph.GetColor()[i]);
   }
+
+  return res_components;
 }
 
 int main() {
@@ -83,6 +92,7 @@ int main() {
   std::cin >> num_vertex >> edge;
   Graph graph(num_vertex + 1);
   Graph graph_reverse(num_vertex + 1);
+
   for (size_t i = 0; i < edge; ++i) {
     int start;
     int finish;
@@ -90,7 +100,19 @@ int main() {
     graph.AddEdge(start, finish);
     graph_reverse.AddEdge(finish, start);
   }
-  graph.TimeCnt();
+
+  TimeCnt(graph, num_vertex + 1);
   graph_reverse.time = graph.time;
-  graph_reverse.CountComponents();
+
+  std::vector<int> res = CountComponents(graph_reverse, num_vertex + 1);
+
+  int cnt_components = 0;
+  for (size_t i = 0; i < res.size(); ++i) {
+    cnt_components = std::max(cnt_components, res[i]);
+  }
+
+  std::cout << cnt_components << "\n";
+  for (size_t i = 0; i < res.size(); ++i) {
+    std::cout << res[i] << ' ';
+  }
 }
